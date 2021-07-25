88fc581c46b222b4b69087597a17fd9ea2a79e16
Hans Sean Nathanael <hans.sean22@gmail.com>
2021-07-25 17:36:20 +0700

    Add custom header bar and menu button

--- app/src/main/java/com/tainka/pvts/MainActivity.kt
+++ app/src/main/java/com/tainka/pvts/MainActivity.kt
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var videoGroup = listOf<List<VideoData>>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+    }
+
+    fun readDataBase()
+    {
+
     }
 }
