7d7fb40bdc302834a2daa16bcf933f7107a04c4b
Hans Sean Nathanael <hans.sean22@gmail.com>
2021-07-25 20:42:20 +0700

    Create login menu and some color setting

--- app/src/main/java/com/tainka/pvts/MainActivity.kt
+++ app/src/main/java/com/tainka/pvts/MainActivity.kt
@@ -4,17 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    var videoGroup = listOf<List<VideoData>>()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-    }
-
-    fun readDataBase()
-    {
-
     }
 }
